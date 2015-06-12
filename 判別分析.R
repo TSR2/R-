f6a98b14ded03c4ad5254e42c9de0fed9fbd4cb6
TSR2 <th0928367718@hotmail.com>
2015-06-12 22:28:25 +0800

    1

--- 判別分析.R
+++ 判別分析.R
@@ -8,12 +8,16 @@ plot(x1,x2)
 text(x1,x2,data$group,adj=-0.5)
 
 plot(x1-mean(x1),x2-mean(x2),pch=21,bg=data$group+1)
-legend("topleft",legen=c("雨天","晴天"),pch=20,bg=c(2,3))
+legend("topleft",legen=c("雨天","晴天"),fill=c(2,3))
+library(MASS)
 ld=lda(data$group~x1+x2)
 names(ld)
 ld
 z=predict(ld)
 z
+table(z$class,data$group)
 abline(0,0.098/0.2296)
-predict(ld,data.frame(x1=8.1,x2=2.0))
+p=c(x1=8.1,x2=2.0)
+
+predict(ld,data.frame(x1=c(8.1,3.9),x2=c(2.0,2.8)))
 
